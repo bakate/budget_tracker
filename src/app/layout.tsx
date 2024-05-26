85e37ff1fee019f5d7f65c0d7e3165b30617a5f7
Bakate <b.ba@startupflow.io>
2024-05-26 18:04:19 +0200

    chore: clone my nextjs template repo and change the primary color

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Budget tracker",
+  description: "Budger Tracker",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <Toaster richColors={true} position="bottom-right" closeButton />
+      </body>
+    </html>
+  );
+}
