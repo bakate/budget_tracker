758d18600020505256d218ed7819bfc0394501c4
Bakate <b.ba@startupflow.io>
2024-05-27 06:38:12 +0200

    fix(drizzle): update DATABASE_URL reference in drizzle.ts

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,7 +1,5 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-import { ENV } from "@/lib/env";
-
-export const sql = neon(ENV.DATABASE_URL!);
+export const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
